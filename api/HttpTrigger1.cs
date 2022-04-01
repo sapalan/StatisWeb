5883ed9bc628d514817d553441133233092d69be
Satheesh Palanisamy <sapalan@microsoft.com>
2022-04-01 16:28:14 +0530

    Test

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -17,7 +17,7 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request. 11");
 
             string name = req.Query["name"];
 
